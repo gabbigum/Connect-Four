feea9c7b3ec13a67390d6a032e1c2018724cb8a8
Gabriel Saliev <gumata.gumata@gmail.com>
2021-03-24 18:59:56 +0200

    add disc insert

--- main.c
+++ main.c
@@ -1,24 +1,72 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define BOARD_WIDTH 7
 #define BOARD_HEIGHT 6
 
+#define PLAYER_1 'A'
+#define PLAYER_2 'B'
+
+
 void displayBoard(char [BOARD_HEIGHT][BOARD_WIDTH]);
+
 void fillBoard(char [BOARD_HEIGHT][BOARD_WIDTH]);
+
+bool insertDisc(char [BOARD_HEIGHT][BOARD_WIDTH], char, int);
 
 
 int main() {
 
     char board[BOARD_HEIGHT][BOARD_WIDTH];
-    // fill board
+
     fillBoard(board);
+
+    // PLAYER A
+    // PLAYER B
+
+    // test disc insertion
+    insertDisc(board, PLAYER_1, 3);
+    insertDisc(board, PLAYER_2, 4);
+    insertDisc(board, PLAYER_1, 3);
+    insertDisc(board, PLAYER_1, 3);
+    insertDisc(board, PLAYER_1, 3);
+
     displayBoard(board);
     return 0;
 }
 
+bool insertDisc(char board[BOARD_HEIGHT][BOARD_WIDTH], char player, int position) {
+    if (position >= BOARD_WIDTH || position <= 0) {
+        return false;
+    }
+    position--;
+    // check if it is at the top of the board
+    if (board[0][position] == PLAYER_1 ||
+        board[0][position] == PLAYER_2) {
+        return false;
+    }
 
+    // iterate over the height
+    int i = 0;
+    while (i < BOARD_HEIGHT) {
+        if (board[i][position] == PLAYER_1 ||
+            board[i][position] == PLAYER_2) {
+            break;
+        }
+        i++;
+    }
+
+    board[i - 1][position] = player;
+
+    return true;
+}
 
 void displayBoard(char board[BOARD_HEIGHT][BOARD_WIDTH]) {
+    for (int i = 1; i <= BOARD_WIDTH; i++) {
+        printf("%d", i);
+    }
+    printf("\n");
+
     for (int i = 0; i < BOARD_HEIGHT; i++) {
         char line[BOARD_WIDTH];
         for (int j = 0; j < BOARD_WIDTH; j++) {
